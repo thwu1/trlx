d4f196994fda8840678d8d39b65035be22e07352
jiaolab-tianhao <wth569749099@gmail.com>
2023-12-20 22:58:23 +0000

    add self implement mistral hydra wrapper

--- examples/hh/utils.py
+++ examples/hh/utils.py
@@ -1,4 +1,10 @@
 from typing import List, Dict
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import math
+from torch import nn
+import os
+from huggingface_hub import snapshot_download
 
 
 def from_openchat_to_llama(str_sample: str) -> str:
@@ -46,6 +52,119 @@ def to_list(str: str, human_template: str, assistant_template: str) -> List[Dict
                 else:
                     output_ls.append({"assistant": new_content})
     return output_ls
+
+
+def fake_reward(samples, *args, **kwargs):
+    return torch.tensor([0.0] * len(samples))
+
+
+def create_reward_fn():  # noqa:  C901
+    class GPTRewardModel(nn.Module):
+        def __init__(self, model_path, eos_token_id, alpha):
+            super().__init__()
+            if "mistral" in model_path or "Llama" in model_path:
+                model = AutoModelForCausalLM.from_pretrained(model_path)
+                self.transformer = model.model
+            else:
+                model = AutoModelForCausalLM.from_pretrained(model_path)
+                self.transformer = model.gpt_neox
+            self.config = model.config
+            # `gpt-neo(x)` models use `hidden_size` attribute names instead of `n_embd``
+            self.config.n_embd = self.config.hidden_size if hasattr(self.config, "hidden_size") else self.config.n_embd
+            self.model = model
+            # self.transformer = model.model
+            self.alpha = alpha
+            self.v_head = nn.Linear(self.config.n_embd, 1, bias=False)
+            self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+            print("Reward tokenizer eos token:", self.tokenizer.eos_token)
+            # self.tokenizer.eos_token_id = eos_token_id
+            self.tokenizer.pad_token = self.tokenizer.unk_token
+            self.PAD_ID = self.tokenizer(self.tokenizer.pad_token)["input_ids"][0]
+            self.K = 7
+
+        def get_device(self):
+            return self.model.device
+
+        def gradient_checkpointing_enable(self):
+            self.model.gradient_checkpointing_enable()
+            return
+
+        def forward(
+            self,
+            input_ids=None,
+            past_key_values=None,
+            attention_mask=None,
+            position_ids=None,
+        ):
+            """
+            input_ids, attention_mask: torch.Size([bs, seq_len])
+            return: scores: List[torch.Size([1])
+            """
+            bs = input_ids.shape[0]
+            transformer_outputs = self.transformer(
+                input_ids,
+                past_key_values=past_key_values,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+            )
+            hidden_states = transformer_outputs[0]
+            scores = []
+            rewards = self.v_head(hidden_states).squeeze(-1)
+            for i in range(bs):
+                c_inds = (input_ids[i] == self.PAD_ID).nonzero()
+                c_ind = c_inds[0].item() if len(c_inds) > 0 else input_ids.shape[1]
+                scores.append(rewards[i, c_ind - 1])
+            return scores
+
+    reward_tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+    reward_model = GPTRewardModel("meta-llama/Llama-2-7b-chat-hf", reward_tokenizer.eos_token_id, 0.5)
+    reward_tokenizer = reward_model.tokenizer
+    print("Reward tokenizer pad token:", reward_tokenizer.pad_token)
+    reward_tokenizer.truncation_side = "left"
+
+    directory = snapshot_download("banghua/refine_rm")
+    for fpath in os.listdir(directory):
+        if fpath.endswith(".pt") or fpath.endswith("model.bin"):
+            checkpoint = os.path.join(directory, fpath)
+            break
+
+    reward_model.load_state_dict(torch.load(checkpoint), strict=False)
+    reward_model.eval()
+    reward_model.requires_grad_(False)
+    if os.environ.get("LOCAL_RANK", "0") == "0":
+        reward_device = torch.cuda.device_count() - 1
+        reward_model = reward_model.to(reward_device)
+        reward_batch_size = 4
+    else:
+        del reward_model
+
+    def get_reward(samples):
+        """samples: List[str]"""
+        input_ids = []
+        attention_masks = []
+        encodings_dict = reward_tokenizer(
+            samples,
+            truncation=True,
+            max_length=2048,
+            padding="max_length",
+            return_tensors="pt",
+        ).to(reward_device)
+        input_ids = encodings_dict["input_ids"]
+        attention_masks = encodings_dict["attention_mask"]
+
+        mbs = reward_batch_size
+        out = []
+        for i in range(math.ceil(len(samples) / mbs)):
+            rewards = reward_model(input_ids=input_ids[i * mbs : (i + 1) * mbs], attention_mask=attention_masks[i * mbs : (i + 1) * mbs])
+            out.extend(rewards)
+        return torch.hstack(out)
+
+    def reward_fn(samples, prompts, **kwargs):
+        samples = [from_openchat_to_llama(sample) for sample in samples]
+        rewards = get_reward(samples)
+        return rewards
+
+    return reward_fn
 
 
 if __name__ == "__main__":
